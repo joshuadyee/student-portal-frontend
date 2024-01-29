e50cd0db013350926b27233a8c08f5a07166b897
Jayeon Hwang <jayeon512@gmail.com>
2024-01-29 15:55:40 -0600

    add experience, education,skill

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,9 +1,53 @@
-export function Resume() {
+// import axios from 'axios'
+// import { useState, useEffect } from 'react'
 
 
-  return (
-    <div>
-      <h1>Resume Page</h1>
-    </div>
-  )
-}
+// export function Resume(studentEmail) {
+//   const [experiance, setExperience] = useState({})
+//   const [education, setEducation] = useState({})
+//   const [skill, setSkill] = useState({})
+//   const [capstone, setCapstone] = useState({})
+
+//   // get student id
+//   // get student data request
+//   // get request using students id
+
+//   const getExperiance = () => {
+//     console.log("getExperiance")
+//     axios
+//       .get(`http://localhost:3000/students.json?email=${studentEmail.studentEmail}`)
+//       .then((response) => {
+//         console.log(response.data)
+//         setStudentId(response.data.id)
+//       })
+//   }
+
+//   const getStudentData = () => {
+//     console.log("getStudentData")
+//     axios
+//       .get(`http://localhost:3000/students/${studentId}`)
+//       .then((response) => {
+//         console.log(response.data)
+//         setStudentData(response.data)
+//       })
+
+//   }
+
+//   useEffect(() => {
+//     getStudentId();
+//   }, []);
+
+//   useEffect(() => {
+//     getStudentData();
+//   }, [getStudentId]);
+
+
+//   return (
+//     <div>
+//       <h1>Resume Page</h1>
+//       <button>Experiance</button>
+//       <button>Education</button>
+//       <button>Skill</button>
+//     </div>
+//   )
+// }
